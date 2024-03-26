426701b8705af1d851371e5451c6966312f5f849
KiriHans <github.mxuxw@slmail.me>
2024-03-26 03:31:15 -0500

    pass 6th stage

--- src/server.cpp
+++ src/server.cpp
@@ -8,12 +8,16 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include <fcntl.h>
 #include <regex>
 #include <unordered_map>
+
+#include <sys/epoll.h>
 
 #include "helpers.cpp"
 
 const int16_t BUFFER_SIZE = 1024;
+const int16_t MAX_EVENTS = 10;
 
 enum http_Code
 {
@@ -25,7 +29,27 @@ std::unordered_map<http_Code, std::string> HTTP_MESSAGE = {
     {OK, "HTTP/1.1 200 OK"},
     {NOT_FOUND, "HTTP/1.1 404 Not Found"}};
 
-bool handle_client(int server_fd, int client_fd)
+void set_nonblocking(int client_fd)
+{
+  // Setting the Socket as "nonblocking"
+  int flags;
+
+  if ((flags = fcntl(client_fd, F_GETFL, 0)) < 0)
+  {
+    std::cerr << "fcntl F_GETFL error" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  flags |= O_NONBLOCK;
+
+  if (fcntl(client_fd, F_SETFL, flags) < 0)
+  {
+    std::cerr << "fcntl F_SETFL error" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+bool handle_client(int client_fd, int epoll_fd)
 {
 
   char buffer[BUFFER_SIZE];
@@ -34,7 +58,17 @@ bool handle_client(int server_fd, int client_fd)
 
   if (client_request <= 0)
   {
-    std::cerr << "Client error" << std::endl;
+     if (client_request == 0)
+    {
+      std::cerr << "Client closed the connection" << std::endl;
+    }
+    else
+    {
+      std::cerr << "An error has ocurred" << std::endl;
+    }
+    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
+    close(client_fd);
+    return false;
   }
 
   std::string str_buffer(buffer);
@@ -43,7 +77,7 @@ bool handle_client(int server_fd, int client_fd)
 
   std::string path_request = "";
   std::string user_agent_request = "";
-  
+
   std::smatch m;
   std::regex e("/([A-Za-z0-9\\-/]*)");
 
@@ -67,27 +101,6 @@ bool handle_client(int server_fd, int client_fd)
 
   path_request = m[0];
 
-  // for (int i = 0; i < str_buffer.size(); i++)
-  // {
-
-  //   if (str_buffer[i] == '\n')
-  //   {
-  //     if (i - 1 > 0 && str_buffer[i - 1] != '\r')
-  //     {
-  //       std::cerr << "Incorrect request" << std::endl;
-  //       return -1;
-  //     }
-
-  //     std::string start_path = str_buffer.substr(0, i - 1);
-
-  //     std::regex_search(start_path, m, e);
-
-  //     path_request = m[0];
-
-  //     break;
-  //   }
-  // }
-
   http_Code code;
   std::string http_status_message;
   std::string http_body_message = "";
@@ -104,8 +117,6 @@ bool handle_client(int server_fd, int client_fd)
 
     http_body_message = path_request.substr(path_request.find("/", 1) + 1);
 
-    std::cout << "body: " << http_body_message << std::endl;
-
     std::string size_echo_message = std::to_string(http_body_message.size());
     http_status_message = HTTP_MESSAGE.at(code) + "\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: " + size_echo_message + "\r\n";
   }
@@ -114,8 +125,6 @@ bool handle_client(int server_fd, int client_fd)
     code = OK;
 
     http_body_message = user_agent_request;
-
-    std::cout << "body: " << http_body_message << std::endl;
 
     std::string size_user_agent_message = std::to_string(http_body_message.size());
     http_status_message = HTTP_MESSAGE.at(code) + "\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: " + size_user_agent_message + "\r\n";
@@ -150,7 +159,7 @@ int main(int argc, char **argv)
   if (server_fd < 0)
   {
     std::cerr << "Failed to create server socket\n";
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   // Since the tester restarts your program quite often, setting REUSE_PORT
@@ -159,7 +168,7 @@ int main(int argc, char **argv)
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
   {
     std::cerr << "setsockopt failed\n";
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   struct sockaddr_in server_addr;
@@ -170,14 +179,14 @@ int main(int argc, char **argv)
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
   {
     std::cerr << "Failed to bind to port 4221\n";
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0)
   {
     std::cerr << "listen failed\n";
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   struct sockaddr_in client_addr;
@@ -185,20 +194,70 @@ int main(int argc, char **argv)
 
   std::cout << "Waiting for a client to connect...\n";
 
-  ssize_t client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+  set_nonblocking(server_fd);
 
-  if (client_fd < 0)
+  // Creating epoll instance and events
+  int epoll_fd = epoll_create1(0);
+
+  if (epoll_fd == -1)
   {
-    std::cerr << "Client failed to connect" << std::endl;
-    close(client_fd);
-    return 1;
+    std::cerr << "Error when creating epoll" << std::endl;
+    exit(EXIT_FAILURE);
   }
 
-  std::cout << "Client connected\n";
+  struct epoll_event ev, events[MAX_EVENTS];
+  ev.events = EPOLLIN;
+  ev.data.fd = server_fd;
 
-  handle_client(server_fd, client_fd);
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev))
+  {
+    std::cerr << "Error when adding server socket to interest list" << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
-  std::cout << "Http status send correctly" << std::endl;
+  // Main loop
+  while (true)
+  {
+    // Waiting for events on the epoll instance "epoll_fd"
+    int wait_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    if (wait_events == -1)
+    {
+      std::cerr << "epoll_wait error" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < wait_events; i++)
+    {
+      if (events[i].data.fd == server_fd)
+      {
+        ssize_t client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+
+        if (client_fd < 0)
+        {
+          std::cerr << "Client failed to connect" << std::endl;
+          close(client_fd);
+          exit(EXIT_FAILURE);
+        }
+
+        std::cout << "Client connected\n";
+
+        set_nonblocking(client_fd);
+
+        ev.events = EPOLLIN | EPOLLET;
+        ev.data.fd = client_fd;
+
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) == -1)
+        {
+          std::cerr << "epoll_ctl while adding client_fd error" << std::endl;
+          exit(EXIT_FAILURE);
+        }
+      }
+      else
+      {
+        handle_client(events[i].data.fd, epoll_fd);
+      }
+    }
+  }
 
   close(server_fd);
 
